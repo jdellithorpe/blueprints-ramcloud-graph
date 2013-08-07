bcfc7229b04a854d4677466e4fc1c6fd00c7099b
Jonathan Ellithorpe <jdellit@stanford.edu>
2013-08-07 15:36:27 -0700

    Adding pom.xml and src directory

---
+++ src/main/java/com/tinkerpop/blueprints/impls/ramcloud/App.java
@@ -0,0 +1,13 @@
+package com.tinkerpop.blueprints.impls.ramcloud;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
