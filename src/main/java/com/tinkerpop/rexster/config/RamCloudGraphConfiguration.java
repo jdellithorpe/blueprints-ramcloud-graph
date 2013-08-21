179424b6a7eb0a95ee5e6151127c4f050c085818
ellitron <jdellit@stanford.edu>
2013-08-21 16:44:40 -0700

    Added rexster as a dependency in the pom, and created a RamCloudGraphConfiguration class that is needed as a part of instantiating a RamCloudGraph in rexster. Not exactly sure yet how to properly extend rexster to support RamCloudGraphs but we're getting there, one step at a time.
    Also removed some extra files

---
+++ src/main/java/com/tinkerpop/rexster/config/RamCloudGraphConfiguration.java
@@ -0,0 +1,16 @@
+package com.tinkerpop.rexster.config;
+
+import com.tinkerpop.blueprints.Graph;
+import com.tinkerpop.blueprints.impls.ramcloud.RamCloudGraph;
+import com.tinkerpop.rexster.config.GraphConfiguration;
+import org.apache.commons.configuration.Configuration;
+
+import java.util.logging.Level;
+
+public class RamCloudGraphConfiguration implements GraphConfiguration {
+
+    public Graph configureGraphInstance(final Configuration properties) throws GraphConfigurationException {
+        return new RamCloudGraph("infrc:host=192.168.1.101,port=12246", Level.INFO);
+    }
+
+}
